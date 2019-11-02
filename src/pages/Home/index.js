e33ace8ec5d79b09fa770d9b5f11de7ade6180c4
Davi augusto <daviaugusto.developer@gmail.com>
2019-11-02 16:39:01 -0300

    header home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Bem-vimdo a pagina Home</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
